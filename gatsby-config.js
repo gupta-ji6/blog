f6b4e22d0be63eea6362b77e17d030ecdfe9b138
gupta-ji6 <ayushgupta197@gmail.com>
2020-05-17 04:41:47 +0530

    :tada: Finally configured logo & colors. Phew!

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,26 +6,26 @@ module.exports = {
     description: `This is my description that will be used in the meta tags and important for search results`,
     hero: {
       heading: `thoughts.`,
-      maxWidth: 652
+      maxWidth: 652,
     },
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/_guptaji_`
+        url: `https://twitter.com/_guptaji_`,
       },
       {
         name: `github`,
-        url: `https://github.com/gupta-ji6`
+        url: `https://github.com/gupta-ji6`,
       },
       {
         name: `instagram`,
-        url: `https://www.instagram.com/_.guptaji._/`
+        url: `https://www.instagram.com/_.guptaji._/`,
       },
       {
         name: `linkedin`,
-        url: `https://www.linkedin.com/in/guptaji6/`
-      }
-    ]
+        url: `https://www.linkedin.com/in/guptaji6/`,
+      },
+    ],
   },
   plugins: [
     {
@@ -36,22 +36,22 @@ module.exports = {
         basePath: "/",
         authorsPage: true,
         sources: {
-          local: true
+          local: true,
           // contentful: true,
-        }
-      }
+        },
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Novela by Narative`,
-        short_name: `Novela`,
+        name: `Blog - Ayush Gupta`,
+        short_name: `Ayush Gupta's Blog`,
         start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#fff`,
+        background_color: `#0A192F`,
+        theme_color: `#0A192F`,
         display: `standalone`,
-        icon: `src/assets/favicon.png`
-      }
-    }
-  ]
+        icon: `src/assets/favicon.png`,
+      },
+    },
+  ],
 };
---
+++ src/@narative/gatsby-theme-novela/gatsby-plugin-theme-ui/colors.ts
@@ -0,0 +1,43 @@
+import prism from "./prism";
+
+export default {
+  prism,
+
+  primary: "#000",
+  secondary: "#73737D",
+  grey: "#73737D",
+  background: "#f0efef",
+  accent: "#192965",
+  hover: "rgba(0, 0, 0, 0.07)",
+  gradient: "linear-gradient(180deg, rgba(212, 221, 255, 0) 0%, #d4ddff 100%)",
+  articleText: "#08080B",
+  track: "rgba(8, 8, 11, 0.3)",
+  progress: "#192965",
+  card: "#fff",
+  error: "#EE565B",
+  success: "#46B17B",
+  errorBackground: "rgba(238, 86, 91, 0.1)",
+  horizontalRule: "rgba(8, 8, 11, 0.15)",
+  inputBackground: "rgba(0, 0, 0, 0.05)",
+  modes: {
+    dark: {
+      grey: "#a0a0b0",
+      primary: "#fafafa",
+      secondary: "#fafafa",
+      accent: "#64FFDA",
+      background: "#0A192F",
+      hover: "rgba(100, 255, 218, 0.20)",
+      gradient:
+        "linear-gradient(180deg, #0A192F 0%, rgba(66, 81, 98, 0.36) 100%)",
+      articleText: "#fafafa",
+      track: "rgba(255, 255, 255, 0.3)",
+      progress: "#64FFDA",
+      card: "#1D2128",
+      error: "#EE565B",
+      success: "#46B17B",
+      errorBackground: "rgba(238, 86, 91, 0.1)",
+      horizontalRule: "rgba(255, 255, 255, 0.15)",
+      inputBackground: "rgba(255, 255, 255, 0.07)",
+    },
+  },
+};
